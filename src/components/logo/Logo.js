df0f6a47fe29c7057cc078dcbac7e485d72a2d09
Hasan Aktas <hasanaktas95@gmail.com>
2021-01-01 07:32:37 +0300

    update

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Logo = (props) => {
-  return <img alt="Logo" src="/static/logo.svg" {...props} />;
+  return (
+    <img
+      alt="Logo"
+      src="static/images/marketplace/hepsiburada.png"
+      style={{ height: 40, width: "auto" }}
+      {...props}
+    />
+  );
 };
 
 export default Logo;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Typography, Button } from "@material-ui/core";
-import { Page } from "components";
+import { Page, Logo } from "components";
 import { useLocale, useProducts } from "hooks";
 const HomePage = () => {
   const { error, loading, products } = useProducts();
@@ -11,6 +11,7 @@ const HomePage = () => {
   const [locale, setLocale] = useLocale();
   return (
     <Page title="Netan Beauty">
+      <Logo />
       <Button onClick={() => setLocale("tr")}>Turkish</Button>
       <Button onClick={() => setLocale("en")}>English</Button>
       <Typography>{locale.home}</Typography>
